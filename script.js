720cf9aa41786d8349a3a373a5a542facbb88280
saikiran-basamsetti <kiransaibasamsetti@gmail.com>
2021-07-06 05:45:01 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    if(principal<=0)
+    {
+        alert("please enter a postive number");
+        return false;
+    }
+    var rate=document.getElementById("rate").value;
+    var years=document.getElementById("years").value;
+    var interest=principal*rate*years/100;
+    var year=new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you depopsit <mark>"+principal+"</mark>\<br\>at an interest of <mark>"+rate+"%</mark>\<br\>You will recieve an amount of <mark>"+interest+"</mark>\<br\>in the year <mark>"+year+"</mark>\<br\>"
+}
